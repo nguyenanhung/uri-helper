3d57cb36f709102c023a665d6c4580dba6597723
nguyenanhung <info@nguyenanhung.com>
2021-09-22 21:50:32 +0700

    create project

---
+++ README.md
@@ -0,0 +1,15 @@
+[![Latest Stable Version](http://poser.pugx.org/nguyenanhung/uri-helper/v)](https://packagist.org/packages/nguyenanhung/uri-helper) [![Total Downloads](http://poser.pugx.org/nguyenanhung/uri-helper/downloads)](https://packagist.org/packages/nguyenanhung/uri-helper) [![Latest Unstable Version](http://poser.pugx.org/nguyenanhung/uri-helper/v/unstable)](https://packagist.org/packages/nguyenanhung/uri-helper) [![License](http://poser.pugx.org/nguyenanhung/uri-helper/license)](https://packagist.org/packages/nguyenanhung/uri-helper) [![PHP Version Require](http://poser.pugx.org/nguyenanhung/uri-helper/require/php)](https://packagist.org/packages/nguyenanhung/uri-helper)
+
+# URI Helper
+
+URI Helper, basic, simple and light
+
+## Contact & Support
+
+If any question & request, please contact following information
+
+| Name        | Email                | Skype            | Facebook      |
+| ----------- | -------------------- | ---------------- | ------------- |
+| Hung Nguyen | dev@nguyenanhung.com | nguyenanhung5891 | @nguyenanhung |
+
+From Vietnam with Love <3
---
+++ src/URI.php
@@ -0,0 +1,490 @@
+<?php
+/**
+ * Project uri-helper
+ * Created by PhpStorm
+ * User: 713uk13m <dev@nguyenanhung.com>
+ * Copyright: 713uk13m <dev@nguyenanhung.com>
+ * Date: 09/22/2021
+ * Time: 21:45
+ */
+
+namespace nguyenanhung\Libraries\URI;
+
+if (!class_exists('nguyenanhung\Libraries\URI\URI')) {
+    /**
+     * Class URI
+     *
+     * @package   nguyenanhung\Libraries\URI
+     * @author    713uk13m <dev@nguyenanhung.com>
+     * @copyright 713uk13m <dev@nguyenanhung.com>
+     */
+    class URI
+    {
+        const SCHEMES_WITH_AUTHORITY = ';http;https;ftp';
+
+        /** @var string */
+        private $scheme;
+
+        /** @var string */
+        private $host;
+
+        /** @var string */
+        private $user;
+
+        /** @var string */
+        private $pass;
+
+        /** @var string */
+        private $path;
+
+        /** @var string */
+        private $query;
+
+        /** @var string */
+        private $fragment;
+
+        /** @var int */
+        private $port;
+
+        /** @var bool */
+        private $absolute = true;
+
+        /**
+         * If $uri is set, we'll hydrate this object with it
+         *
+         * @param string $uri {optional}
+         */
+        public function __construct($uri = null)
+        {
+            if ($uri !== null) {
+                $this->fromString($uri);
+            }
+        }
+
+        /**
+         * Alias for getUri.
+         *
+         * @return string
+         */
+        public function __toString()
+        {
+            return $this->getUri();
+        }
+
+        /**
+         * Hydrate this object with values from a string
+         *
+         * @param $uri
+         *
+         * @return self
+         */
+        public function fromString($uri)
+        {
+            if (is_numeric($uri)) { //Could be a valid url
+                $uri = '' . $uri;
+            }
+            if (!is_string($uri)) {
+                $uri = '';
+            }
+            $this->setRelative();
+            if (0 === strpos($uri, '//')) {
+                $this->setAbsolute();
+            }
+            $parsed_url = parse_url($uri);
+            if (!$parsed_url) {
+                return $this;
+            }
+            if (array_key_exists('scheme', $parsed_url)) {
+                $this->setAbsolute();
+            }
+            foreach ($parsed_url as $urlPart => $value) {
+                $method = 'set' . ucfirst($urlPart);
+                if (method_exists($this, $method)) {
+                    $this->$method($value);
+                }
+            }
+
+            return $this;
+        }
+
+        /**
+         * Get the URI from the set parameters
+         *
+         * @return string
+         */
+        public function getUri()
+        {
+            $userPart = '';
+            if ($this->getUser() !== null) {
+                if ($this->getPass() !== null) {
+                    $userPart = $this->getUser() . ':' . $this->getPass() . '@';
+                } else {
+                    $userPart = $this->getUser() . '@';
+                }
+            }
+            $schemePart = ($this->getScheme() ? $this->getScheme() . '://' : '//');
+            if (!in_array($this->getScheme(), self::getSchemesWithAuthority(), true)) {
+                $schemePart = $this->getScheme() . ':';
+            }
+            $portPart     = ($this->getPort() ? ':' . $this->getPort() : '');
+            $queryPart    = ($this->getQuery() ? '?' . $this->getQuery() : '');
+            $fragmentPart = ($this->getFragment() ? '#' . $this->getFragment() : '');
+            if ($this->isRelative()) {
+                return $this->getPath() .
+                       $queryPart .
+                       $fragmentPart;
+            }
+            $path    = $this->getPath();
+            $pathLen = strlen($path);
+            if (0 !== $pathLen && '/' !== $path[0]) {
+                $path = '/' . $path;
+            }
+
+            return $schemePart .
+                   $userPart .
+                   $this->getHost() .
+                   $portPart .
+                   $path .
+                   $queryPart .
+                   $fragmentPart;
+        }
+
+        /**
+         * @param string $fragment
+         *
+         * @return self
+         */
+        public function setFragment($fragment)
+        {
+            $this->fragment = $fragment;
+
+            return $this;
+        }
+
+        /**
+         * @return string
+         */
+        public function getFragment()
+        {
+            return $this->fragment;
+        }
+
+        /**
+         * @param string $host
+         *
+         * @return self
+         */
+        public function setHost($host)
+        {
+            $this->host = $host;
+            $this->setAbsolute();
+
+            return $this;
+        }
+
+        /**
+         * @return string
+         */
+        public function getHost()
+        {
+            return $this->host;
+        }
+
+        /**
+         * @param string $pass
+         *
+         * @return self
+         */
+        public function setPass($pass)
+        {
+            $this->pass = $pass;
+
+            return $this;
+        }
+
+        /**
+         * @return string
+         */
+        public function getPass()
+        {
+            return $this->pass;
+        }
+
+        /**
+         * @param string $path
+         *
+         * @return self
+         */
+        public function setPath($path)
+        {
+            $this->path = $path;
+
+            return $this;
+        }
+
+        /**
+         * @return string
+         */
+        public function getPath()
+        {
+            return $this->path;
+        }
+
+        /**
+         * Set the query. Must be a string, and the prepending "?" will be trimmed.
+         * Example: ?a=b&c[]=123 -> "a=b&c[]=123"
+         *
+         * @param string $query
+         *
+         * @return self
+         * @see Sensimity_Helper_UriTest::provideSetQuery
+         *
+         */
+        public function setQuery($query)
+        {
+            $this->query = null;
+            if (is_string($query)) {
+                $this->query = ltrim($query, '?');
+            }
+
+            return $this;
+        }
+
+        /**
+         * @return string
+         */
+        public function getQuery()
+        {
+            return $this->query;
+        }
+
+        /**
+         * Set the scheme. If its empty, it will be set to null.
+         *
+         * Must be a string. Can only contain "a-z A-Z 0-9 . : -".
+         * Will be forced to lowercase.
+         * Appended : or // will be removed.
+         *
+         * @param string $scheme
+         *
+         * @return self
+         * @see Sensimity_Helper_UriTest::provideSetScheme
+         *
+         */
+        public function setScheme($scheme)
+        {
+            $this->scheme = null;
+            if (empty($scheme)) {
+                return $this;
+            }
+            $schemePattern = '/[^a-zA-Z0-9\.\:\-]/';
+            $scheme        = preg_replace($schemePattern, '', $scheme);
+            $scheme        = strtolower($scheme);
+            $scheme        = rtrim($scheme, ':/');
+            $scheme        = trim($scheme, ':/');
+            $scheme        = str_replace('::', ':', $scheme);
+            $schemeLen     = strlen($scheme);
+            if ($schemeLen !== 0) {
+                if ($this->isRelative()) {
+                    /* Explained: */
+                    /* @see Sensimity_Helper_UriTest::testRelativeAbsoluteUrls */
+                    $exp = explode('/', ltrim($this->getPath(), '/'));
+                    $this->setHost($exp[0]);
+                    unset($exp[0]);
+                    $this->setPath(null);
+                    $path    = implode('/', $exp);
+                    $pathLen = strlen($path);
+                    if ($pathLen > 0) {
+                        //Only create the "/" if theres a path
+                        $this->setPath('/' . $path);
+                    }
+                    $this->setAbsolute();
+                }
+                $this->scheme = $scheme;
+            }
+
+            return $this;
+        }
+
+        /**
+         * @return string
+         */
+        public function getScheme()
+        {
+            return $this->scheme;
+        }
+
+        /**
+         * @param string $user
+         *
+         * @return self
+         */
+        public function setUser($user)
+        {
+            $this->user = $user;
+
+            return $this;
+        }
+
+        /**
+         * @return string
+         */
+        public function getUser()
+        {
+            return $this->user;
+        }
+
+        /**
+         * Port must be a valid number. Otherwise it will be set to NULL. (default scheme port)
+         *
+         * @param int|string $port
+         *
+         * @return self
+         * @see Sensimity_Helper_UriTest::provideSetPort
+         *
+         */
+        public function setPort($port)
+        {
+            $this->port = null;
+            if ((is_string($port) || is_numeric($port)) && ctype_digit((string) $port)) {
+                $this->port = (int) $port;
+            }
+
+            return $this;
+        }
+
+        /**
+         * @return int
+         */
+        public function getPort()
+        {
+            return $this->port;
+        }
+
+        /**
+         * @return bool
+         */
+        public function isRelative()
+        {
+            return (!$this->absolute);
+        }
+
+        /**
+         * @return bool
+         */
+        public function isAbsolute()
+        {
+            return ($this->absolute);
+        }
+
+        /**
+         * @return $this
+         */
+        public function setAbsolute()
+        {
+            $this->absolute = true;
+
+            return $this;
+        }
+
+        /**
+         * @return $this
+         */
+        public function setRelative()
+        {
+            $this->absolute = false;
+
+            return $this;
+        }
+
+        /** Some helpful static functions */
+
+        /**
+         * @param      $uri
+         * @param null $scheme
+         *
+         * @return string
+         */
+        public static function changeScheme($uri, $scheme = null)
+        {
+            if ($scheme === null) { //null for scheme = just no change at all - only in this static function, for BC!
+                return $uri;
+            }
+            $class = static::class;
+            /** @var object|mixed $uri */
+            $uri = new $class($uri);
+            $uri->setScheme($scheme);
+
+            return $uri->getUri();
+        }
+
+        /**
+         * Function getSchemesWithAuthority
+         *
+         * @return false|string[]
+         * @author   : 713uk13m <dev@nguyenanhung.com>
+         * @copyright: 713uk13m <dev@nguyenanhung.com>
+         * @time     : 08/18/2021 54:11
+         *
+         * @see      http://tools.ietf.org/html/rfc3986#section-3
+         */
+        public static function getSchemesWithAuthority()
+        {
+            return explode(';', self::SCHEMES_WITH_AUTHORITY);
+        }
+
+        /**
+         * Function isSchemeLess
+         *
+         * @return bool
+         * @author   : 713uk13m <dev@nguyenanhung.com>
+         * @copyright: 713uk13m <dev@nguyenanhung.com>
+         * @time     : 08/18/2021 54:07
+         */
+        public function isSchemeLess()
+        {
+            $scheme = $this->getScheme();
+
+            return ($this->isRelative() || ($this->isAbsolute() && empty($scheme)));
+        }
+
+        /**
+         * Header Redirect
+         *
+         * Header redirect in two flavors
+         * For very fine grained control over headers, you could use the Output
+         * Library's set_header() function.
+         *
+         * @param string $uri       URL
+         * @param string $method    Redirect method
+         *                          'auto', 'location' or 'refresh'
+         * @param int    $code      HTTP Response status code
+         *
+         * @return    void
+         *
+         * @copyright https://www.codeigniter.com/
+         */
+        public static function redirect($uri = '', $method = 'auto', $code = null)
+        {
+            // IIS environment likely? Use 'refresh' for better compatibility
+            if ($method === 'auto' && isset($_SERVER['SERVER_SOFTWARE']) && strpos($_SERVER['SERVER_SOFTWARE'], 'Microsoft-IIS') !== false) {
+                $method = 'refresh';
+            } elseif ($method !== 'refresh' && ($code === null || !is_numeric($code))) {
+                if (isset($_SERVER['SERVER_PROTOCOL'], $_SERVER['REQUEST_METHOD']) && $_SERVER['SERVER_PROTOCOL'] === 'HTTP/1.1') {
+                    $code = ($_SERVER['REQUEST_METHOD'] !== 'GET')
+                        ? 303    // reference: http://en.wikipedia.org/wiki/Post/Redirect/Get
+                        : 307;
+                } else {
+                    $code = 302;
+                }
+            }
+            if ($method === 'refresh') {
+                header('Refresh:0;url=' . $uri);
+            } else {
+                header('Location: ' . $uri, true, $code);
+            }
+
+            exit;
+        }
+    }
+}
