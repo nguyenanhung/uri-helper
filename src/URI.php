363e3846782a9e513af51a1cab38897e64dbcd58
nguyenanhung <info@nguyenanhung.com>
2022-07-22 03:10:42 +0700

    Release v2.0.1

--- src/URI.php
+++ src/URI.php
@@ -466,10 +466,14 @@ if (!class_exists('nguyenanhung\Libraries\URI\URI')) {
          */
         public static function redirect(string $uri = '', string $method = 'auto', int $code = null)
         {
+            if (!preg_match('#^(\w+:)?//#i', $uri) && function_exists('site_url')) {
+                $uri = site_url($uri);
+            }
+
             // IIS environment likely? Use 'refresh' for better compatibility
             if ($method === 'auto' && isset($_SERVER['SERVER_SOFTWARE']) && strpos($_SERVER['SERVER_SOFTWARE'], 'Microsoft-IIS') !== false) {
                 $method = 'refresh';
-            } elseif ($method !== 'refresh' && ($code === null || !is_numeric($code))) {
+            } elseif ($method !== 'refresh' && (empty($code) or !is_numeric($code))) {
                 if (isset($_SERVER['SERVER_PROTOCOL'], $_SERVER['REQUEST_METHOD']) && $_SERVER['SERVER_PROTOCOL'] === 'HTTP/1.1') {
                     $code = ($_SERVER['REQUEST_METHOD'] !== 'GET')
                         ? 303    // reference: http://en.wikipedia.org/wiki/Post/Redirect/Get
@@ -478,12 +482,15 @@ if (!class_exists('nguyenanhung\Libraries\URI\URI')) {
                     $code = 302;
                 }
             }
-            if ($method === 'refresh') {
-                header('Refresh:0;url=' . $uri);
-            } else {
-                header('Location: ' . $uri, true, $code);
-            }
 
+            switch ($method) {
+                case 'refresh':
+                    header('Refresh:0;url=' . $uri);
+                    break;
+                default:
+                    header('Location: ' . $uri, true, $code);
+                    break;
+            }
             exit;
         }
     }
