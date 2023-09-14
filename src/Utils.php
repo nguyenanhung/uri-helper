3b89bb400503baf4776a2964af814347c7884a7c
nguyenanhung <info@nguyenanhung.com>
2023-09-14 10:18:31 +0700

    Update Project at Thu Sep 14 10:18:31 +07 2023

---
+++ src/Utils.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Project uri-helper
+ * Created by PhpStorm
+ * User: 713uk13m <dev@nguyenanhung.com>
+ * Copyright: 713uk13m <dev@nguyenanhung.com>
+ * Date: 14/09/2023
+ * Time: 10:13
+ */
+
+namespace nguyenanhung\Libraries\URI;
+
+class Utils
+{
+    public static function reformatParseUrl($url = '', $domain = '')
+    {
+        $url = trim($url);
+        $domain = trim($domain);
+        if (empty($url)) {
+            return $url;
+        }
+        $url = trim($url);
+        $parse = parse_url($url);
+        if (isset($parse['host'])) {
+            return $url;
+        }
+        $newUrl = $domain . $url;
+        return trim($newUrl);
+    }
+}
